f9a5d9812f2dfa0c0084f3fe67d41b34252ef3b3
Emjliy <6p304s8@code.edu.az>
2022-04-06 00:59:26 +0400

    task added

---
+++ UpDownCast/Extensionmethods.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace UpDownCast
+{
+    static class Extensionmethods
+    {
+        public static void Customcontains(this string par, string word)
+        {
+            Console.WriteLine(par.Contains(word));
+
+        }
+        public static void Customcontains(this string par, char ch)
+        {
+            for (int i = 0; i < par.Length; i++)
+            {
+                if (ch == par[i])
+                {
+                    Console.WriteLine("char is exist in string ");
+                    return;
+                }
+            }
+
+            Console.WriteLine("char is not exist in string");
+
+        }
+        public static void IsPrime(this int num)
+        {
+            if (num==1)
+            {
+                Console.WriteLine("hech biri");
+            }
+            else
+            {
+                int count = 2;
+                for (int i = count; i < num; i++)
+                {
+                    if (num%i==0)
+                    {
+                        count++;
+                    }
+                }
+                if (count==2)
+                {
+                    Console.WriteLine("num is prime");
+                    return;
+                }
+            }
+            Console.WriteLine("num is not prime");
+        }
+
+    }
+}
+
+
---
+++ UpDownCast/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace UpDownCast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Extensionmethods.Customcontains("emilya", "emi");
+            Extensionmethods.Customcontains("emidlya", 'z');
+            Extensionmethods.IsPrime(17);
+        }
+    }
+
+}
